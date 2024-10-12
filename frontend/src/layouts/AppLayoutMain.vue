c179c50c6ab1aba7fa9b08a135a1190bcbd5ec76
Andrew Smirnov <smirnov.andrew.1999@yandex.ru>
2024-10-12 20:43:45 +0300

    feat: add LoginView

--- frontend/src/layouts/AppLayoutMain.vue
+++ frontend/src/layouts/AppLayoutMain.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="app_layout">
+    <AppHeader />
     <slot />
   </div>
 </template>
+
+<script setup>
+import AppHeader from "../common/components/AppHeader.vue";
+</script>
 
 <style lang="scss" scoped>
 @import "@/assets/scss/app.scss";
