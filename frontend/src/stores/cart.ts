393c12ac55f6c083621b812374c821e7cc7b2ed2
Andrew Smirnov <smirnov.andrew.1999@yandex.ru>
2024-10-16 21:47:46 +0300

    feat: add getters for calc price

--- frontend/src/stores/cart.ts
+++ frontend/src/stores/cart.ts
@@ -40,9 +40,46 @@ export const useCartStore = defineStore("cart", {
         price: 170,
       },
     ],
+    choosedMiscs: [
+      {
+        miscId: 1,
+        quantity: 1,
+      },
+    ],
+    choosedPizzas: [
+      {
+        name: "",
+        sauceId: 1,
+        doughId: 1,
+        quantity: 1,
+        price: 100,
+        sizeId: 1,
+        ingredients: Array.from({ length: 30 }, (_, i) => i + 1).map((i) => ({
+          ingredientId: i,
+          quantity: 0,
+        })),
+      },
+    ],
   }),
   getters: {
     getMisc: (state) => state.misc,
+
+    getPrice: (state) => {
+      let price = 0;
+
+      price += state.choosedMiscs
+        .map(
+          (choosedMisc) =>
+            (state.misc.find((v) => v.id == choosedMisc.miscId)?.price ?? 0) *
+            choosedMisc.quantity
+        )
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+      price += state.choosedPizzas
+        .map((choosedPizza) => choosedPizza.price * choosedPizza.quantity)
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+      return price;
+    },
   },
   actions: {},
 });
--- frontend/src/stores/pizza.ts
+++ frontend/src/stores/pizza.ts
@@ -260,6 +260,16 @@ export const usePizzaStore = defineStore("pizza", {
         multiplier: 3,
       },
     ],
+    choosed: {
+      name: "",
+      sauceId: 1,
+      doughId: 1,
+      sizeId: 1,
+      ingredients: Array.from({ length: 30 }, (_, i) => i + 1).map((i) => ({
+        ingredientId: i,
+        quantity: 0,
+      })),
+    },
   }),
   getters: {
     getDough: (state) =>
@@ -274,6 +284,27 @@ export const usePizzaStore = defineStore("pizza", {
       })),
     getSauces: (state) => state.sauces,
     getSizes: (state) => state.sizes,
+
+    getPrice: (state) => {
+      let price = 0;
+
+      price +=
+        state.dough.find((v) => v.id == state.choosed.doughId)?.price ?? 0;
+      price +=
+        state.sauces.find((v) => v.id == state.choosed.sauceId)?.price ?? 0;
+      price += state.choosed.ingredients
+        .map(
+          (choosedIngredient) =>
+            (state.ingredients.find(
+              (v) => v.id == choosedIngredient.ingredientId
+            )?.price ?? 0) * choosedIngredient.quantity
+        )
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+      price *=
+        state.sizes.find((v) => v.id == state.choosed.sizeId)?.multiplier ?? 1;
+
+      return price;
+    },
   },
   actions: {},
 });
