771848a4691ac17462defed93c84226c23dba9e9
Andrew Smirnov <smirnov.andrew.1999@yandex.ru>
2024-11-21 19:38:22 +0300

    fix: update services

--- frontend/src/services/AddressService.js
+++ frontend/src/services/AddressService.js
@@ -1,5 +1,6 @@
 import { HttpClient } from "./HttpClient";
 import httpProvider from "./providers";
+import { getToken } from "./token-manager";
 
 const BASE_URL = "/api/addresses";
 
@@ -11,21 +12,21 @@ class AddressService extends HttpClient {
       throw Error(e);
     }
   }
-  async createPost(post) {
+  async create(data) {
     try {
-      return this.post("/", { data: post });
+      return this.post("/", { data });
     } catch (e) {
       throw Error(e);
     }
   }
-  async updatePost(post) {
+  async update(data) {
     try {
-      return this.put(`/${post.id}`, { data: post });
+      return this.put(`/${data.id}`, { data });
     } catch (e) {
       throw Error(e);
     }
   }
-  async deletePost(id) {
+  async delete(id) {
     try {
       await this.delete(`/${id}`);
     } catch (e) {
@@ -37,4 +38,5 @@ class AddressService extends HttpClient {
 export default new AddressService({
   httpProvider,
   baseURL: BASE_URL,
+  getToken,
 });
